48d899893908ca3c7b0d4c64e846f3ee021798f9
LucasFImhof <lucasf.imhof@gmail.com>
2024-12-04 15:50:19 -0300

    Refatorado o teste Login Incorreto utilizando um dublê

--- cypress/e2e/Login-Incorreto.cy.js
+++ cypress/e2e/Login-Incorreto.cy.js
@@ -1,14 +1,21 @@
 describe('Página de login', () => {
   beforeEach(() => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/'); // Acessa o site de estudo Adopet
-    cy.get('[data-test="login-button"]').click(); //Clica no botão de login
+    cy.visit('https://adopet-frontend-cypress.vercel.app');
+    cy.get('[data-test="login-button"]').click();
+    cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
+      statusCode: 400,
+    }).as('stubPost')
   })
-  it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
-    cy.get('[data-test="input-loginEmail"]').type('Marjorie3'); // Preenche o e-mail do usuário
-    cy.get('[data-test="input-loginPassword"]').type('123'); // Preenche a senha do usuário
-    cy.get('[data-test="submit-button"]').click(); // Clica no botão para realizar o login
-    cy.contains('Por favor, verifique o email digitado').should('be.visible') // Verifica se contem a mensagem de erro "Por favor, verifique o e-mail digitado"
-    cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible') // Verifica se contem a mensagem de erro "A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres"
 
+  it('Verifica mensagem de falha no login', () => {
+    cy.get('[data-test="submit-button"]').click()
+    cy.contains('É necessário informar um endereço de email').should('be.visible')
+    cy.contains('Insira sua senha').should('be.visible')
+  })
+
+  it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+    cy.login('ana@email.com', 'Senha123')
+    cy.wait('@stubPost')
+    cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible')
   })
 })
